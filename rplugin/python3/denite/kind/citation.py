95b44751a64cd7815275cedc8129dbfea957f3cc
DancingQuanta <andytheseeker@gmail.com>
2018-08-26 15:18:00 +0100

    Denite citation kind to reuse existing denite kinds' actions

--- rplugin/python3/denite/kind/citation.py
+++ rplugin/python3/denite/kind/citation.py
@@ -1,16 +1,19 @@
 import os
 import re
-from .word import Kind as Base
+from .word import Kind as Word
+from .command import Kind as Command
+from .file import Kind as File
+from .directory import Kind as Directory
 
 
-class Kind(Base):
+class Kind(Word, Command, File, Directory):
 
     def __init__(self, vim):
         super().__init__(vim)
 
         self.name = 'citation'
         self.default_action = 'default'
-        self.persist_actions = ['preview']
+        # self.persist_actions = ['preview']
 
     def action_default(self, context):
         target = context['targets'][0]
@@ -19,7 +22,7 @@ class Kind(Base):
         if source_context['__source'] is 'sub_sources':
             self.action_field(context)
         else:
-            self.action_append(context)
+            Word.action_append(self, context)
 
     def action_field(self, context):
         target = context['targets'][0]
@@ -27,18 +30,14 @@ class Kind(Base):
         self.vim.command(cmd)
 
     def action_open(self, context):
-        target = context['targets'][0]
-        path = target['action__path']
-
-        if (re.match('https?://', path)
-            or os.path.splitext(path)[1] == ".pdf"):
+        for target in context['targets']:
+            path = target['action__path']
             self.vim.call('denite#util#open', path)
 
     def action_directory(self, context):
-        target = context['targets'][0]
-        path = target['action__path']
-        self.vim.call('denite#util#open', path)
+        Directory.action_open(self, context)
 
     def action_preview(self, context):
-        pass
-
+        target = context['targets'][0]
+        if 'action__command' in target:
+            Command.action_execute(self, context)
